9adff2d2719b04bc7f291bfd24fc19c40fdd598e
xingxuan tan <565041366@qq.com>
2020-06-03 09:25:05 +0800

    123

---
+++ client/src/dva.tsx
@@ -0,0 +1,35 @@
+import { create } from 'dva-core'
+import { createLogger } from 'redux-logger'
+import createLoading from 'dva-loading'
+
+let app, store, dispatch, registered
+
+function createApp(options?: any) {
+  const { models } = options
+  if (process.env.NODE_ENV === 'development') {
+    // options.onAction = [createLogger()]
+    // console.log(options)
+  }
+  app = create({
+    ...options
+  })
+  app.use(createLoading({}))
+
+  if (!registered) models.forEach((model) => app.model(model))
+  registered = true
+  app.start()
+
+  store = app._store
+  app.getStore = () => store
+
+  dispatch = store.dispatch
+  app.dispatch = dispatch
+  return app
+}
+
+export default {
+  createApp,
+  getDispatch() {
+    return app.dispatch
+  }
+}
